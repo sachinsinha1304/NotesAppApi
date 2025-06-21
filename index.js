e7233e4bcfbb74de8200f417ae93056e0ff328f5
Sachin Sinha <sinha6031@gmail.com>
2025-06-21 20:51:05 +0530

    Adding cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 require('dotenv').config();
 
 const notesRoutes = require('./routes/noteRoute');
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/notes', notesRoutes);
 
 const PORT = process.env.PORT || 3000;
